98118ee3963778a764b73310b4a5c8edbe31a486
Omar Elgendy <omaratef3221@gmail.com>
2024-06-11 18:49:43 +0400

    Latest modifications

--- get_model.py
+++ get_model.py
@@ -2,7 +2,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 
 
-def get_qwen_model(model_id = "Qwen/Qwen2-1.5B-Instruct"):
+def get_qwen_model(model_id = "Qwen/Qwen2-0.5B-Instruct"):
     tokenizer = AutoTokenizer.from_pretrained(model_id)
     model = AutoModelForCausalLM.from_pretrained(model_id)
     return tokenizer, model
--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 import warnings
 warnings.filterwarnings("ignore")
 
-
+import argparse
 from trl import SFTTrainer, DataCollatorForCompletionOnlyLM
 from transformers import TrainingArguments
 from get_model import get_qwen_model
 from data import get_dataset, prepare_data
 
-def main():
-    tokenizer, model = get_qwen_model(model_id = "Qwen/Qwen2-1.5B-Instruct")
-    data = get_dataset(dataset_id="motherduckdb/duckdb-text2sql-25k")
+def main(args):
+    tokenizer, model = get_qwen_model(model_id = args.model_id)
+    data = get_dataset(dataset_id=args.dataset_id)
     
     training_params = TrainingArguments(
     output_dir="./model",
     save_strategy="no",
-    num_train_epochs=1,
-    per_device_train_batch_size=4,
+    num_train_epochs=args.epochs,
+    per_device_train_batch_size=args.batch_size,
     optim="paged_adamw_32bit",
     save_steps=0,
     logging_steps=500,
     learning_rate=1e-4,
-    push_to_hub_model_id = "omaratef3221/Qwen2-1.5B-Instruct-SQL-generator"
+    push_to_hub = True,
+    push_to_hub_model_id = f"{args.model_id.split('/')[1]}-SQL-generator"
     )
     
     response_template = " ### The response query is:"
@@ -34,10 +35,17 @@ def main():
     )
     
     trainer.train()
-    trainer.save_model("./qwen_model_1.5B")
-    trainer.save_tokenzer("./qwen_tokenizer_1.5B")
+    trainer.save_model(f"./{args.model_id.split('/')[1]}")
+    trainer.save_tokenzer(f"./{args.model_id.split('/')[1]}")
     
-    trainer.push_to_hub("omaratef3221/Qwen2-1.5B-Instruct-SQL-generator")
+    trainer.push_to_hub(f"omaratef3221/{args.model_id.split('/')[1]}-SQL-generator")
     
 if __name__ == "__main__":
-    main()
+    # model_id, dataset_id, epochs, batch_size, push_to_hub_model_id
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_id', type=str, default='Qwen/Qwen2-0.5B-Instruct')
+    parser.add_argument('--dataset_id', type=str, default='motherduckdb/duckdb-text2sql-25k')
+    parser.add_argument('--epochs', type=int, default=5)
+    parser.add_argument('--batch_size', type=int, default=8)
+    args = parser.parse_args()
+    main(args)
