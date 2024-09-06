1298f597f4e7978096fee0cec8e0b064fcfe6d0f
Omar Elgendy <omaratef3221@gmail.com>
2024-09-06 21:27:47 +0400

    z

--- get_model.py
+++ get_model.py
@@ -7,7 +7,7 @@ def get_qwen_model(model_id = "Qwen/Qwen2-0.5B-Instruct"):
     
     if model_id == "Omaratef3221/qwen-0.5-rbf-mlp":
         model = CustomQwen2ForCausalLM.from_pretrained("Omaratef3221/qwen-0.5-rbf-mlp")
-        tokenizer = AutoTokenizer.from_pretrained("Omaratef3221/qwen-0.5-rbf-mlp")
+        tokenizer = AutoTokenizer.from_pretrained("Omaratef3221/qwen-0.5-rbf-mlp", force_download = True)
     else:
         tokenizer = AutoTokenizer.from_pretrained(model_id)
         model = AutoModelForCausalLM.from_pretrained(model_id)
